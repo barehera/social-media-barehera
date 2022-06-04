1019dc9323033afbeb3090e9813a8442e15bbbc8
Cagan Buyukavcilar <mircagi@hotmail.com>
2022-06-04 23:20:54 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbYIz1Amn6QP39KfvwdEvImeX7UTN6k6I",
-  authDomain: "instagram-clone-c9ee1.firebaseapp.com",
-  projectId: "instagram-clone-c9ee1",
-  storageBucket: "instagram-clone-c9ee1.appspot.com",
-  messagingSenderId: "814122607711",
-  appId: "1:814122607711:web:5c5b7447943354aeb427ea",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
